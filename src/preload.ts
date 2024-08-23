d700c2fd4f851df0df2b3d6a5e53e37dcfec2197
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-23 22:08:31 +0530

    implement baasic logic to load UI dynamically

--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,11 @@
+import { SystemGeneralInformation } from "./modules/system";
+
 class BuddhiliveSysinfoUI {
 
   private listItems = ['General', 'System'];
+  private appRoot: HTMLDivElement = document.querySelector('#app-root');
+
+  private systemUI = new SystemGeneralInformation(this.appRoot);
 
   createWindow() {
     const listView: HTMLUListElement = document.querySelector('#app-navbar');
@@ -11,10 +16,13 @@ class BuddhiliveSysinfoUI {
       const anchorElement = document.createElement('a');
       anchorElement.classList.add('nav-link');
       anchorElement.innerHTML = item;
+      anchorElement.setAttribute('data-view', item.toLowerCase());
       anchorElement.addEventListener('click', (evt: MouseEvent) => this.onNavSelected(evt));
       listElement.append(anchorElement);
       listView.append(listElement);
     });
+
+    (listView.childNodes[1].childNodes[0] as HTMLAnchorElement).click();
   }
 
   onNavSelected(evt: MouseEvent) {
@@ -22,6 +30,17 @@ class BuddhiliveSysinfoUI {
     const listItems = document.querySelectorAll('#app-navbar .nav-item .nav-link');
     listItems.forEach(item => item.classList.remove('active'));
     selectedNav.classList.add('active');
+
+    const activeView: string = selectedNav.dataset['view'];
+    
+    switch (activeView) {
+      case 'general':
+        this.systemUI.loadUI();
+        break;
+    
+      default:
+        break;
+    }
   }
 }
 
