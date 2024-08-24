c8cd7c8ddf7700553c27f4f6e6e38c3bae9b648b
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-24 22:29:38 +0530

    Add baseboard information

--- src/modules/system.ts
+++ src/modules/system.ts
@@ -1,24 +1,29 @@
-import { system, bios } from "systeminformation";
+import { system, bios, baseboard } from "systeminformation";
 import { SysInfoTemplateGenerator } from "./template-generator";
 
 export class SystemGeneralInformation {
     constructor(private appRoot: HTMLDivElement) { }
 
     loadUI() {
-        system()
-            .then((data) => {
-                const templateGen = new SysInfoTemplateGenerator();
-                const tableView = templateGen.createBasicTable(data, 'System Information');
-                this.appRoot.appendChild(tableView);
-            })
-            .catch((error) => console.error(error));
+        // System Information
+        system().then((data) => {
+            const templateGen = new SysInfoTemplateGenerator();
+            const tableView = templateGen.createBasicTable(data, 'System Information');
+            this.appRoot.appendChild(tableView);
+        }).catch((error) => console.error(error));
 
-        bios()
-            .then((data) => {
-                const templateGen = new SysInfoTemplateGenerator();
-                const tableView = templateGen.createBasicTable(data, 'BIOS Information');
-                this.appRoot.appendChild(tableView);
-            })
-            .catch((error) => console.error(error));
+        // BIOS Information
+        bios().then((data) => {
+            const templateGen = new SysInfoTemplateGenerator();
+            const tableView = templateGen.createBasicTable(data, 'BIOS Information');
+            this.appRoot.appendChild(tableView);
+        }).catch((error) => console.error(error));
+
+        // Baseboard Information
+        baseboard().then((data) => {
+            const templateGen = new SysInfoTemplateGenerator();
+            const tableView = templateGen.createBasicTable(data, 'Baseboard Information');
+            this.appRoot.appendChild(tableView);
+        }).catch((error) => console.error(error));
     }
 }
