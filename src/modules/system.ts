ae846592d1608076fada0f94890d1b0e07923a7d
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-24 22:27:10 +0530

    Add Bios information

--- src/modules/system.ts
+++ src/modules/system.ts
@@ -1,4 +1,4 @@
-import { system } from "systeminformation";
+import { system, bios } from "systeminformation";
 import { SysInfoTemplateGenerator } from "./template-generator";
 
 export class SystemGeneralInformation {
@@ -9,6 +9,14 @@ export class SystemGeneralInformation {
             .then((data) => {
                 const templateGen = new SysInfoTemplateGenerator();
                 const tableView = templateGen.createBasicTable(data, 'System Information');
+                this.appRoot.appendChild(tableView);
+            })
+            .catch((error) => console.error(error));
+
+        bios()
+            .then((data) => {
+                const templateGen = new SysInfoTemplateGenerator();
+                const tableView = templateGen.createBasicTable(data, 'BIOS Information');
                 this.appRoot.appendChild(tableView);
             })
             .catch((error) => console.error(error));
